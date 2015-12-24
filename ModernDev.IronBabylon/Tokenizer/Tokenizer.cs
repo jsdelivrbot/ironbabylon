d06dfd44a3ca3fa67ccab6428da5c70726420099
Bohdan Shtepan <winsatana@ya.ru>
2015-12-24 12:03:25 +0200

    Parser\Tokenizer.cs merged to Tokenizer\Tokenizer.cs

--- ModernDev.IronBabylon/Tokenizer/Tokenizer.cs
+++ ModernDev.IronBabylon/Tokenizer/Tokenizer.cs
@@ -1201,6 +1201,32 @@ namespace ModernDev.IronBabylon
 			}
 		}
 
+		/// <summary>
+		/// Raise an unexpected token error.
+		/// </summary>
+		protected bool Unexpected(int? pos = null)
+		{
+			Raise(pos ?? State.Start, "Unexpected token");
+
+			return false;
+		}
+
+		/// <summary>
+		/// This function is used to raise exceptions on parse errors. It
+		/// takes an offset integer (into the current `input`) to indicate
+		/// the location of the error, attaches the position to the end
+		/// of the error message, and then raises a `SyntaxError` with that
+		/// message.
+		/// </summary>
+		protected void Raise(int pos, string msg)
+		{
+			var loc = GetLineInfo(Input, pos);
+
+			msg += $" ({loc.Line}:{loc.Column})";
+
+			throw new SyntaxErrorException(msg, loc, pos);
+		}
+
 		#endregion
 	}
 }
