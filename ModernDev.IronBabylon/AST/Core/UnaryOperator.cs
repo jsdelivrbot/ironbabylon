45517656dd3d46c25dffa53976e1176e157a6c28
Bohdan Shtepan <winsatana@ya.ru>
2016-02-05 02:26:27 +0200

    Add UnaryOperator enum

---
+++ ModernDev.IronBabylon/AST/Core/UnaryOperator.cs
@@ -0,0 +1,13 @@
+namespace ModernDev.IronBabylon
+{
+    public enum UnaryOperator
+    {
+        UnaryNegation, // -
+        UnaryPlus, // +
+        LogicalNot, // !
+        BitwiseNot, // ~
+        Typeof, // typeof
+        Void, // void
+        Delete, // delete
+    }
+}
