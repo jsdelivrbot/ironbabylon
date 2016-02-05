bfa0571b1a87605af38b65ae6827a9234eee845c
Bohdan Shtepan <winsatana@ya.ru>
2016-02-05 02:25:43 +0200

    Add BinaryOperator enum

---
+++ ModernDev.IronBabylon/AST/Core/BinaryOperator.cs
@@ -0,0 +1,27 @@
+namespace ModernDev.IronBabylon
+{
+    public enum BinaryOperator
+    {
+        Equal, // ==
+        NotEqual, // !=
+        StrictEqual, // ===
+        StrictNotEqual, // !==
+        LessThan, // <
+        LessThanOrEqual, // <=
+        GreaterThan, // >
+        GreaterThanOrEqual, // >=
+        LeftShift, // <<
+        RightShift, // >>
+        UnsignedRightShift, // >>>
+        Addition, // +
+        Substraction, // -
+        Multiplication, // *
+        Division, // /
+        Remainder, // %
+        BitwiseOR, // |
+        BitwiseXOR, // ^
+        BitwiseAND, // &
+        In, // in
+        Instanceof, // instanceof
+    }
+}
